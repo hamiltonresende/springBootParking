1190a898604ec59dc327cf573a145c71fe7dd2e0
Hamilton Resende <hamilton.rferreira@gmail.com>
2022-08-10 20:15:59 -0300

    Iniciando o projeto de controle de estacionamento utilizando o springBoot. O banco utilizado será o Sql Server.

---
+++ src/main/java/com/api/springParkingControl/SpringParkingControlApplication.java
@@ -0,0 +1,13 @@
+package com.api.springParkingControl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringParkingControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringParkingControlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/springParkingControl/SpringParkingControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.springParkingControl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringParkingControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
