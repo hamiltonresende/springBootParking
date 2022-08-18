bf17a1df83889b406bf52392dfacc38feecd922d
Hamilton Resende <hamilton.rferreira@gmail.com>
2022-08-17 22:05:17 -0300

    Testando a inicialização da aplicaçao.

--- src/main/java/com/api/springParkingControl/SpringParkingControlApplication.java
+++ src/main/java/com/api/springParkingControl/SpringParkingControlApplication.java
@@ -2,12 +2,20 @@ package com.api.springParkingControl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringParkingControlApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringParkingControlApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String index() {
+		return "Initiating test spring boot";
 	}
 
 }
